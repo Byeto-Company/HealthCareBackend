8607fa911315ed0d10e33e3a74e22235c009ad7d
AmirHossein Shirazi <ashzih2@gmail.com>
2024-10-11 22:34:34 +0330

    some fixes and improvements

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -5,7 +5,7 @@ from .models import RequestDemo, ContactUs
 class RequestDemoSerializer(serializers.ModelSerializer):
     class Meta:
         model = RequestDemo
-        fields = ['id', 'full_name', 'email', 'phone_number', 'company_name', 'message', 'requested_at']
+        fields = ['id', 'full_name', 'email', 'phone_number', 'company_name', 'module_name', 'message', 'requested_at']
 
 
 class ContactUsSerializer(serializers.ModelSerializer):
