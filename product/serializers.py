d75ce3f7ca78f580b89e52433efbca2d53782f8e
Parsa Nazer <parsa.nazerrr@gmail.com>
2024-10-14 22:07:35 +0330

    mamaliz.clown.com

--- product/serializers.py
+++ product/serializers.py
@@ -83,9 +83,9 @@ class ProductDetailSerializer(serializers.ModelSerializer):
 
     def get_meta(self, obj):
         data = {
-            'meta_title': obj.meta_title,
-            'meta_keywords': obj.meta_keyword,
-            'meta_description': obj.meta_description,
+            'title': obj.meta_title,
+            'keywords': obj.meta_keyword,
+            'description': obj.meta_description,
         }
         return data
 
